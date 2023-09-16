24d7a24b959080a4d5d7130bb1ec6089e0aa6864
Bernice <ashexne@gmail.com>
2023-09-16 22:36:04 +0100

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to MoviePlay, a movie database website! 
+        Search for your favourite movies and their ratings, and add 
+        them to your list of movies to watch later. Use the searchbar function to look for 
+        any movie you like. Have fun!
+      </p>
+
+    </div>
+  );
+}
